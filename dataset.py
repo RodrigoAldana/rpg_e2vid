b2af41d1498cecf2c975d1346cc2e22b6583a1db
Irene Pérez <71775618+ireneperezsalesa@users.noreply.github.com>
2021-02-22 11:43:54 +0100

    Update dataset.py
    Modified to use CropParameters for frames and flow as well as events, so that padding size is automatically computed for all of them.

--- dataset.py
+++ dataset.py
@@ -52,7 +52,7 @@ class EventData:
             if f.endswith('npy'):
                 self.flow.append(f)
 
-    def get_item(self, index):
+    def get_item(self, index, num_encoders):
         #Take next event/image (index+1) in order to use warp to index -> index+1 to obtain warped image at index+1
         #The flow of index->index+1 is at flow[index]
         event_name = os.path.join(self.event_dir, self.event_tensors[index+1])
@@ -61,31 +61,26 @@ class EventData:
 
         event_array = numpy.load(event_name)
         event_tensor = torch.tensor(event_array)
-
-        frame = cv2.imread(frame_name)
-
-        flow_array = numpy.load(flow_name)
-
-        frame_tensor = torch.tensor(numpy.transpose(frame, (2, 0, 1)))
-        frame_tensor = frame_tensor.type(torch.FloatTensor)
-
         events = event_tensor.unsqueeze(dim=0)
         events = self.event_preprocessor(events)
         events = self.crop.pad(events)
 
-        pady = torch.zeros((3, 2, 240))
-        frame_t = torch.cat((pady, frame_tensor, pady), 1)
-        frame_t = frame_t.unsqueeze(dim=0)
+        frame = cv2.imread(frame_name)
+        frame_tensor = torch.tensor(numpy.transpose(frame, (2, 0, 1)))
+        frame_tensor = frame_tensor.type(torch.FloatTensor)
+        frame_t = frame_tensor.unsqueeze(dim=0)
+        frame_t = self.crop.pad(frame_t)
 
-        flow_tensor = torch.tensor(numpy.transpose(flow_array, (1,2,0)))
+        flow_array = numpy.load(flow_name)
+        flow_tensor = torch.tensor(flow_array)
         flow_tensor = flow_tensor.type(torch.FloatTensor)
-        padf = torch.zeros((2, 240, 2))
-        flow = torch.cat((padf, flow_tensor, padf), 0)
-        flow = flow.unsqueeze(dim=0)
+        flow = flow_tensor.unsqueeze(dim=0)
+        flow = self.crop.pad(flow)
+        flow = flow.permute(0, 2, 3, 1)
 
         return events, frame_t, flow
 
-def flow_warp(x, flow, interp_mode='bilinear', padding_mode='zeros'):
+def flow_warp(x, flow, interp_mode='bilinear', padding_mode='border'):
     """Warp an image or feature map with optical flow
     Args:
         x (Tensor): size (N, C, H, W)
@@ -96,20 +91,22 @@ def flow_warp(x, flow, interp_mode='bilinear', padding_mode='zeros'):
     Returns:
         Tensor: warped image or feature map
     """
-    assert x.size()[-2:] == flow.size()[1:3]
-    B, C, H, W = x.size()
-    # mesh grid
-    grid_y, grid_x = torch.meshgrid(torch.arange(0, H), torch.arange(0, W))
 
+    B, C, H, W = x.size()
+    assert x.size()[-2:] == flow.size()[1:3]
+
+    # Mesh grid
+    grid_y, grid_x = torch.meshgrid(torch.arange(0, H), torch.arange(0, W))
     grid = torch.stack((grid_x, grid_y), 2).float()  # W(x), H(y), 2
     grid.requires_grad = False
-    grid = grid.type_as(x)
+    grid = grid.type_as(x) # If x is a tensor in 'cuda:0', grid is too
     vgrid = grid + flow
 
-    # scale grid to [-1,1]
+    # Scale grid to [-1,1]
     vgrid_x = 2.0 * vgrid[:, :, :, 0] / max(W - 1, 1) - 1.0
     vgrid_y = 2.0 * vgrid[:, :, :, 1] / max(H - 1, 1) - 1.0
     vgrid_scaled = torch.stack((vgrid_x, vgrid_y), dim=3)
 
     output = grid_sample(x, vgrid_scaled, mode=interp_mode, padding_mode=padding_mode)
+
     return output
